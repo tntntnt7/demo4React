64c73036508079967b4e741b327ea1e2a4a9bca5
niko <tntntnt7@gmail.com>
2019-04-03 01:35:13 +0800

    todo item

---
+++ src/components/todo-item/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import './style.scss'
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+
+interface ITodoItem {
+	title:			string
+	content:		string
+	createTime: string
+	endTime:		string
+	onClick:		() => void
+}
+
+@observer
+export default class TodoItem extends React.Component<ITodoItem, {}> {
+
+	@observable public left: string
+
+	public componentWillMount(): void {
+		const { endTime } = this.props
+		// new Date()
+	}
+
+	public render(): JSX.Element {
+		const { title, content, createTime, endTime, onClick } = this.props
+		return (
+			<div className='todo-item' onClick={onClick}>
+				<div className='text'>
+					<div className='title'>{title}</div>
+					<div>{content}</div>
+				</div>
+				<div className='time'>
+					<div>{createTime}</div>
+					<div>{endTime}</div>
+				</div>
+				<div className='left'>{this.left}</div>
+			</div>
+		)
+	}
+}
